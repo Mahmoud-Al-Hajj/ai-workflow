38b45045cff857e0a475b761e1ef6c5c9e6402ba
mah00j <mah06.hajj@gmail.com>
2025-08-30 18:19:30 +0300

    Refactor validation middleware to enhance error messaging and ensure user uniqueness during registration

--- backend/src/middleware/validationMiddleware.js
+++ backend/src/middleware/validationMiddleware.js
@@ -1,5 +1,6 @@
 import { body, param, validationResult } from "express-validator";
-
+import { UserDBService } from "../services/database/userDBService.js";
+const userDBService = new UserDBService();
 export const validateCreateWorkflow = [
   body("description").isString().notEmpty(),
   body("n8nApiKey").isString().notEmpty(),
@@ -23,11 +24,19 @@ export const validateIdParam = [
 ];
 
 export const validateRegister = [
-  body("name").isString().notEmpty(),
-  body("email").isEmail().notEmpty(),
-  body("password").isString().notEmpty(),
+  body("name").isString().notEmpty().withMessage("name is required"),
+  body("email")
+    .isEmail()
+    .notEmpty()
+    .custom(async (value) => {
+      const existingUser = await userDBService.getUserByEmail(value);
+      if (existingUser) {
+        throw new Error("A user already exists with this e-mail address");
+      }
+    }),
+  body("password").isString().notEmpty().withMessage("password is required"),
   body("n8nUrl").isString().notEmpty(),
-  body("n8nApiKey").isString().notEmpty(),
+  body("n8nApiKey").isString().notEmpty().withMessage("n8nApiKey is required"),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -38,8 +47,8 @@ export const validateRegister = [
 ];
 
 export const validateLogin = [
-  body("email").isEmail().notEmpty(),
-  body("password").isString().notEmpty(),
+  body("email").isEmail().notEmpty().withMessage("email is required"),
+  body("password").isString().notEmpty().withMessage("password is required"),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
