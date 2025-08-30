2d5421b29430e2e384cc5aac4bf66f97d35721c6
mah00j <mah06.hajj@gmail.com>
2025-08-30 18:11:45 +0300

    Add validation middleware for workflow creation, registration, and login

---
+++ backend/src/middleware/validationMiddleware.js
@@ -0,0 +1,52 @@
+import { body, param, validationResult } from "express-validator";
+
+export const validateCreateWorkflow = [
+  body("description").isString().notEmpty(),
+  body("n8nApiKey").isString().notEmpty(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+export const validateIdParam = [
+  param("id").isInt(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+export const validateRegister = [
+  body("name").isString().notEmpty(),
+  body("email").isEmail().notEmpty(),
+  body("password").isString().notEmpty(),
+  body("n8nUrl").isString().notEmpty(),
+  body("n8nApiKey").isString().notEmpty(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+export const validateLogin = [
+  body("email").isEmail().notEmpty(),
+  body("password").isString().notEmpty(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+//export const validateUpdateUser = [];
